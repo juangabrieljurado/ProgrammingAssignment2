0cb26f490cef112c7d16ec40a86af992cff6a848
juangabrieljurado <juangabrieljurado@gmail.com>
2018-02-13 21:25:07 +0100

    My answer for this assignment
    I write the result for this assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+# This function, makeCacheMatrix, makes a cache matrix from a given x.
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        # I set the value of the matrix.
+        CacheMatrix <-NULL
+        setMatrix <- function(y){
+                x <<- y
+                CacheMatrix <<- NULL
+        }
+        
+        #Then, I return the matrix.
+        getMatrix <- function () x
+        
+        #After that, I set the cache thought a 'setCache' function.
+        setCache <- function(inverse) CacheMatrix <<- inverse
+        
+        #Finally, I get the cache inversed in the following function.
+        getCache <- function () CacheMatrix
+        list(setMatrix = setMatrix, getMatrix = getMatrix,
+             setCache = setCache,
+             getCache = getCache)
 }
 
 
-## Write a short comment describing this function
-
+#This function 'cacheSolve' allows me to get a matrix with the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        #First, I define 'cacheMatrix' and get the cache.
+        cacheMatrix <- x$getCache()
+        
+        #Then, it checks if the cachematrix is calculated. If so, it gets the cachematrix.
+        if (!is.null(cacheMatrix)){
+                message("loading cache matrix...")
+                return(CacheMatrix)
+        }
+        #Otherwise, it calculates the value thought setCache function.
+        else {
+                data <- x$getMatrix()
+                CacheMatrix <- solve(data,...)
+                x$setCache(CacheMatrix)
+                return(CacheMatrix)
+        }
 }
